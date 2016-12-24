a1e26f3b93f9e9566087c2dbbdafbbc2fb92e6f6
Riccardo Campo <riccardo.cmp94@yahoo.it>
2016-12-24 17:38:05 +0100

    Prototype1 program (#1)

---
+++ Prototype1Controller/Prototype1Controller/BluetoothManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype1Controller
+{
+    public class BluetoothManager
+    {
+        SerialPort BTPort;
+        Buffer input_buffer;
+        Buffer output_buffer;
+        public event EventHandler BTInputReceived;
+
+
+        public BluetoothManager(SerialPort port, Buffer in_buff, Buffer out_buff)                
+        {
+            input_buffer = in_buff;
+            output_buffer = out_buff;
+            BTPort = port;
+        }
+
+
+        // Open the BTport and associate each event to its handler.
+        public void InitBTM(Parser p)
+        {
+            BTPort.Open();
+            BTPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            BTInputReceived += p.BTInputReceivedHandler;                                
+        }
+
+        // On delete I close the port, you never know...
+        ~BluetoothManager()                         
+        {
+            BTPort.Close();
+        }
+
+        // Returns the input reveived.
+        public string SerialRead()                  
+        {
+            string command = BTPort.ReadLine();
+            return command;
+        }
+
+        // Send message to the bluetooth output buffer.
+        public void SerialWrite(string command)    
+        {
+            try
+            {
+                command = command + "\r";
+                /*foreach (char c in command){
+                    BTPort.Write(c.ToString());
+
+                }*/
+                BTPort.WriteLine(command);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: " + ex.Message);
+            }
+        }
+
+        // Send the parsed command to Teo.
+        public void CommandParsedHandler (object sender, EventArgs e)
+        {
+            string command = output_buffer.Pop();
+            Console.WriteLine("Sending message: " + command);
+            SerialWrite(command);
+        }
+
+        // Push the message received from BT input buffer to input buffer.
+        public void DataReceivedHandler (object sender, SerialDataReceivedEventArgs e)
+        {
+            string command = SerialRead();                             
+            System.Console.WriteLine("Recieved message: " + command);
+            input_buffer.Push(command);
+            OnBTInputReceived(EventArgs.Empty);
+        }
+
+        protected virtual void OnBTInputReceived(EventArgs e)
+        {
+            BTInputReceived?.Invoke(this, e);                   // Delegate call (check reference on events).
+        }
+
+    }
+}
---
+++ Prototype1Controller/Prototype1Controller/Parser.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype1Controller
+{
+    // This class will manage the input commands both from Bluetooth and Xbox controller.
+    // After being signaled of an input received Parser will parse the command and decide which 
+    // decision to take and which command to send to the BT manager.
+    public class Parser
+    {        
+        Buffer bt_input_buffer;
+        Buffer xb_input_buffer;
+        Buffer output_buffer;
+        public event EventHandler CommandParsed;
+        public Parser(Buffer bt_in_buff, Buffer xb_in_buff, Buffer out_buff)
+        {
+            bt_input_buffer = bt_in_buff;
+            xb_input_buffer = xb_in_buff;
+            output_buffer = out_buff;
+        }
+
+        public void InitParser(BluetoothManager btm)
+        {
+            CommandParsed += btm.CommandParsedHandler;                      // Associating CommanParsed event to its Handler.
+        }
+
+        // Extracts the command from the XB input buffer and call the XB parser.
+        public void XBInputReceivedHandler(object sender, EventArgs e)
+        {
+            string command = xb_input_buffer.Pop();
+            ParseXB(command);
+        }
+
+        // Extracts the command from the BT input buffer and call the BT parser.
+        public void BTInputReceivedHandler(object sender, EventArgs e)
+        {
+            string command = bt_input_buffer.Pop();
+            ParseBT(command);            
+        }
+
+        // Parses a command redirecting it to the adequate method.
+        private void ParseBT(string command)
+        {/*
+            switch (command)
+            {
+                case "non-echo":
+                    break;
+                default:
+                    EchoTest(command);
+                    break;
+            }
+            */
+        }
+
+        private void ParseXB(string command)
+        {
+            if( command == "A")
+            {
+                output_buffer.Push("move 0 -1 0");
+                OnCommandParsed(EventArgs.Empty);
+            }
+
+            // Parse the input received from the xbox LStick the x-axis value refers to the strage speed
+            // the y-asix value to the forward speed (note that a value of -1 equals to moving forward)
+            if (command.StartsWith("M"))
+            {
+
+                command = command.Split('M')[1];
+                string[] comm_args = command.Split(' ');
+
+                double x = ParseMotorSpeed(comm_args[0], 1.4, 1);           // Strafe parsing
+                double y = ParseMotorSpeed(comm_args[1], -2, -1);           // Forward parsing
+                double z = ParseMotorSpeed(comm_args[2], -4, -3);           // Angular parsing
+
+                output_buffer.Push("move " + x + " " + y + " " + z);
+                OnCommandParsed(EventArgs.Empty);
+
+            }
+            //XBTest(command);
+
+        }
+
+        private string EchoTest(string command)
+        {
+            command = "parsed command: " + command;
+            output_buffer.Push(command);
+            OnCommandParsed(EventArgs.Empty);
+            return command;
+        }
+
+        protected virtual void OnCommandParsed(EventArgs e)
+        {
+            CommandParsed?.Invoke(this, e);                     // Delegate call (check reference on events).
+        }
+        
+        // Associates to each stick value a corrispondent value in speed, currently supporting 2 
+        // speed values, fast and slow. These need to be set accordingly to the different movement
+        // functionality (forward, strafe, angular).
+        private double ParseMotorSpeed(string stick_value, double fast, double slow)
+        {
+            double speed;
+            switch (stick_value)
+            {
+                case "100":
+                    speed = fast;
+                    break;
+
+                case "75":
+                    speed = slow;
+                    break;
+
+                case "50":
+                    speed = 0;
+                    break;
+
+                case "25":
+                    speed = -slow;
+                    break;
+
+                case "0":
+                    speed = -fast;
+                    break;
+
+                case "85":
+                    speed = fast;
+                    break;
+
+                case "67":
+                    speed = slow;
+                    break;
+
+                case "33":
+                    speed = -slow;
+                    break;
+
+                case "15":
+                    speed = -fast;
+                    break;
+                default:
+                    speed = 0;
+                    break;
+            }
+            return speed;
+        }
+    }
+}
