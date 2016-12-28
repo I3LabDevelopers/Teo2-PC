446b5e2d52a79e02890f092d7d90d3fd87cccd9a
Riccardo Campo <riccardo.cmp94@yahoo.it>
2016-12-28 19:22:57 +0100

    Prototype2 (#2)
    * Prototype1 program (#1)
    
    * prototype2

---
+++ Teo2 Control App/Teo2 Control App/Parser.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prototype1Controller
+{
+    // This class will manage the input commands both from Bluetooth and Xbox controller.
+    // After being signaled of an input received Parser will parse the command and decide which 
+    // decision to take and which command to send to the BT manager.
+    public class Parser
+    {        
+        Buffer bt_input_buffer;
+        Buffer xb_input_buffer;
+        Buffer output_buffer;
+        public event EventHandler CommandParsed;
+        Dictionary<string, string> parsing_map;
+
+        // Some actions require to stop the output of certain commands releted to movement. A timeout 
+        // is set to cover the case of unblock message from Teo2 lost.
+
+        bool motion_block = false;                                  
+        int motion_block_timer = 0;
+        int motion_block_timeout = int.Parse(Program.config["motion_block_timeout"]);
+
+        public Parser(Buffer bt_in_buff, Buffer xb_in_buff, Buffer out_buff)
+        {
+            bt_input_buffer = bt_in_buff;
+            xb_input_buffer = xb_in_buff;
+            output_buffer = out_buff;
+        }
+
+        public void InitParser(BluetoothManager btm, Dictionary<string, string> preset)
+        {
+            CommandParsed += btm.CommandParsedHandler;                      // Associating CommanParsed event to its Handler.
+            parsing_map = preset;
+        }
+
+        // Extracts the command from the XB input buffer and call the XB parser.
+        public void XBInputReceivedHandler(object sender, EventArgs e)
+        {
+            string command = xb_input_buffer.Pop();
+            ParseXB(command);
+        }
+
+        // Extracts the command from the BT input buffer and call the BT parser.
+        public void BTInputReceivedHandler(object sender, EventArgs e)
+        {
+            string command = bt_input_buffer.Pop();
+            ParseBT(command);            
+        }
+
+        // Parses a command redirecting it to the adequate method.
+        private void ParseBT(string command)
+        {
+            if (command == "end motion")
+                motion_block = false;
+        }
+
+        private void ParseXB(string command)
+        {
+            // Release the block if the time is out.
+            if (Math.Abs(DateTime.Now.Second - motion_block_timer) >= motion_block_timeout)
+                motion_block = false;
+            
+            if (!motion_block)
+            {
+                switch (command)
+                {
+                    case "A":
+                        SetMood("scared");
+                        break;
+
+                    case "B":
+                        SetMood("angry");
+                        break;
+
+                    case "X":
+                        SetMood("sad");
+                        break;
+
+                    case "Y":
+                        SetMood("happy");
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (command.StartsWith("M"))
+                    SetMotorsSpeed(command);
+            }
+
+            if (parsing_map.ContainsKey(command))
+            {
+                command = parsing_map[command];
+                switch (command)
+                {
+                    case "ciao":
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        private string EchoTest(string command)
+        {
+            command = "parsed command: " + command;
+            output_buffer.Push(command);
+            OnCommandParsed(EventArgs.Empty);
+            return command;
+        }
+
+        protected virtual void OnCommandParsed(EventArgs e)
+        {
+            CommandParsed?.Invoke(this, e);                     // Delegate call (check reference on events).
+        }
+        
+        private void SetMood(string mood)
+        {
+            output_buffer.Push(mood);
+            OnCommandParsed(EventArgs.Empty);
+            motion_block = true;
+            motion_block_timer = DateTime.Now.Second;
+        }
+
+        // Parse the input received from the xbox LStick the x-axis value refers to the strage speed
+        // the y-asix value to the forward speed (note that a value of -1 equals to moving forward)
+        private void SetMotorsSpeed(string command)
+        {
+            command = command.Split('M')[1];
+            string[] comm_args = command.Split(' ');
+
+            double x = ParseMotorSpeed(comm_args[0], 1.4, 1);           // Strafe parsing
+            double y = ParseMotorSpeed(comm_args[1], -2, -1);           // Forward parsing
+            double z = ParseMotorSpeed(comm_args[2], -4, -3);           // Angular parsing
+
+            output_buffer.Push("move " + x + " " + y + " " + z);
+            OnCommandParsed(EventArgs.Empty);
+        }
+
+        // Associates to each stick value a corrispondent value in speed, currently supporting 2 
+        // speed values, fast and slow. These need to be set accordingly to the different movement
+        // functionality (forward, strafe, angular).
+        private double ParseMotorSpeed(string stick_value, double fast, double slow)
+        {
+            double speed;
+            switch (stick_value)
+            {
+                case "100":
+                    speed = fast;
+                    break;
+
+                case "75":
+                    speed = slow;
+                    break;
+
+                case "50":
+                    speed = 0;
+                    break;
+
+                case "25":
+                    speed = -slow;
+                    break;
+
+                case "0":
+                    speed = -fast;
+                    break;
+
+                case "85":
+                    speed = fast;
+                    break;
+
+                case "67":
+                    speed = slow;
+                    break;
+
+                case "33":
+                    speed = -slow;
+                    break;
+
+                case "15":
+                    speed = -fast;
+                    break;
+                default:
+                    speed = 0;
+                    break;
+            }
+            return speed;
+        }
+    }
+}
